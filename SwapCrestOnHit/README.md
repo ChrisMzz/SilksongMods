54c341f40481e6a0245dfe3d3ee202e23d0c1b33
ChrisMzz <christopher.mazzerbo@gmail.com>
2025-12-25 11:44:35 +0100

    add crestSanity to SwapCrestOnHit config menu

--- SwapCrestOnHit/README.md
+++ SwapCrestOnHit/README.md
@@ -1,12 +1,14 @@
 # SwapCrestOnHit
 
 Swaps Hornet's crest every time she takes damage.
-Currently swaps Hornet's active crest only with other unlocked crests. The mod ensures that the chest changes, so typically if you have two crests unlocked the crest will cycle between the two in a predictable way.
+Swaps Hornet's active crest with other unlocked crests or any other crest in the game (off by default, can be toggled in config menu). The mod ensures that the chest changes, so typically if you have two crests unlocked the crest will cycle between the two in a predictable way.
 
 The loadout of each crest is *per-crest*, so you will alternate loadouts as you change crest when taking damage.
 
+Configurable: You can toggle a `Crest Sanity`. It's on by default, in which case the mod will swap only with other unlocked crests. If you turn it off, ANY other crest in the game can be selected, whether that be Shaman, Cloakless, Cursed, or any of the Hunter Crest evolutions (which, yes, are separate crests internally). Use this at your own risk, if you only want pure chaos.
+
 Plans for the future maybe:
- - Add a config option to swap with *any other valid crest in the game*, including Cloakless and Cursed crests, regardless of unlock status or in-game logic. 
+ - ~~Add a config option to swap with *any other valid crest in the game*, including Cloakless and Cursed crests, regardless of unlock status or in-game logic.~~
 
 ## Install
 
--- SwapCrestOnHit/SwapCrestOnHit.cs
+++ SwapCrestOnHit/SwapCrestOnHit.cs
@@ -1,19 +1,26 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using GlobalSettings;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
 
-[BepInPlugin("com.chrismzz.swapcrestonhit", "Swap Crest On Hit", "1.0.0")]
+[BepInPlugin("com.chrismzz.swapcrestonhit", "Swap Crest On Hit", "1.1.0")]
 
 public class SwapCrestOnHit : BaseUnityPlugin
 {
     public static readonly Random rnd = new Random();
+    internal static ConfigEntry<bool> crestSanity;
     private void Awake()
     {
 
         Logger.LogInfo("SwapCrestOnHit loaded and initialized.");
-        
+        crestSanity = Config.Bind(
+            "General",
+            "Crest Sanity",
+            true,
+            "Only use unlocked and available crests. Set to false if you want pure chaos."
+        );
 
 
         Harmony.CreateAndPatchAll(typeof(SwapCrestOnHit), null);
@@ -23,6 +30,7 @@ public class SwapCrestOnHit : BaseUnityPlugin
     [HarmonyPostfix]
     private static void TakeDamagePostfix(HeroController __instance)
     {
+        int currentSilk = __instance.playerData.silk;
         List<ToolCrest> allCrests = ToolItemManager.GetAllCrests();
         List<ToolCrest> availableCrests = new List<ToolCrest>();
         int crestCount = ToolItemManager.GetUnlockedCrestsCount();
@@ -31,20 +39,27 @@ public class SwapCrestOnHit : BaseUnityPlugin
             if (!crest.IsHidden && crest.IsBaseVersion && crest.IsUnlocked)
                 availableCrests.Add(crest);
         }
-
-        if (crestCount > 1 && __instance.playerData.CurrentCrestID != Gameplay.CursedCrest.name && __instance.playerData.CurrentCrestID != Gameplay.CloaklessCrest.name)
+        bool canSwap = (crestCount > 1 && __instance.playerData.CurrentCrestID != Gameplay.CursedCrest.name && __instance.playerData.CurrentCrestID != Gameplay.CloaklessCrest.name);
+        List<ToolCrest> crestsList = crestSanity.Value ? availableCrests : allCrests;
+        int count = crestSanity.Value ? crestCount : allCrests.Count;
+        if (!crestSanity.Value)
+            crestsList.Add(Gameplay.CursedCrest);
+        if (canSwap || !crestSanity.Value)
         {
             __instance.ResetAllCrestState();
-            int crestIdx = rnd.Next(0, crestCount);
-            ToolCrest newCrest = availableCrests[crestIdx];
+            int crestIdx = rnd.Next(0, count);
+            ToolCrest newCrest = crestsList[crestIdx];
             while (__instance.playerData.CurrentCrestID == newCrest.name)
             {
-                crestIdx = rnd.Next(0, crestCount);
-                newCrest = availableCrests[crestIdx];
+                crestIdx = rnd.Next(0, count);
+                newCrest = crestsList[crestIdx];
             }
             __instance.playerData.CurrentCrestID = newCrest.name;
             ToolItemManager.SendEquippedChangedEvent(force: true);
         }
+        // sometimes ResetAllCrestState seems to remove silk from the player
+        __instance.playerData.silk = currentSilk;
+
     }
 
  }
